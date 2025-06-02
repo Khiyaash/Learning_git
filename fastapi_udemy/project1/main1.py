6fcd198a1d9cda7c373e667a06b6af48da95c1ea
Sangeetha <sangeethasm1902@gmail.com>
2025-06-02 19:26:45 +0530

    creating main1.py

--- fastapi_udemy/project1/main1.py
+++ fastapi_udemy/project1/main1.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class Car(BaseModel):
+    make: Optional[str]
+    model: Optional[str]
+    year: Optional[int] = Field(...,ge=1970,lt=2022)
+    price: Optional[float]
+    engine: Optional[str] = "V4"
+    autonomous: Optional[bool]
+    sold: Optional[List[str]]
 
 app = FastAPI()
 
